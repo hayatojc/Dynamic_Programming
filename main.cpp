66679920c7034bd9b0428865c4a1c21d35c83211
Jose Chavez <chavez.joseluis89@gmail.com>
2017-11-11 18:25:03 -0500

    Reads text file and output correctly

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
 
 //Struct to hold the values required in each array element
@@ -10,35 +13,42 @@ struct vector2
 
 int main() {
 
-    //Dummy data for original matrix
-    const int row = 2;
-    const int col = 4;
-
-    int testMatrix[row][col] =
+    //file read
+    ifstream inFile;
+    inFile.open("input-small.txt");
+    if (!inFile)
     {
-        {2,6,1,2},
-        {5,6,7,8}
-    };
+    cerr << "Unable to open file input-small.txt";
+    exit(1);
+    }
+    int row = 0;
+    int col = 0;
+    inFile >> row >> col;
+    int testMatrix[row][col];
 
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < col; j++)
+            inFile >> testMatrix[i][j];
+
+    //declares i and j for all future uses in loops
     int i;
     int j;
 
     vector2 newMatrix[row][col];
 
-    //Fills Original Matrix with Dummy Data above
-    cout << "Test Matrix:" << endl; //Print out Original Matrix
-
+    //Print out Original Matrix
+    cout << "Test Matrix:" << endl;
     for (i = 0; i < row; i++)
         {
             for(j = 0; j < col; j++)
             {
-                cout << testMatrix[i][j] << "  ";
+                cout << setw(3) << testMatrix[i][j] << "  ";
             }
             cout << endl;
         }
 
-    cout << "New Matrix: " << endl;
-
+    //New Matrix
+    cout << endl << "New Matrix: " << endl;
     for (i = 0; i < row; i++) //Pull in weights for first row of weighted matrix
     {
         for(j = 0; j < col; j++)
@@ -51,19 +61,18 @@ int main() {
 
     for (i = 1; i < row; i++) //Pull in weights for remaining rows of weighted matrix
     {
-
         for(j = 0; j < col; j++)
         {
             //If in left-most column (only compare up and to right)
-            int up = testMatrix[i-1][j];
+            int up = newMatrix[i-1][j].weightSum;
             int right = 0;
-            if (j != col) right = ((testMatrix[i - 1][j + 1]) * 1.4);
+            if (j != col) right = ((newMatrix[i - 1][j + 1].weightSum) * 1.4);
             int left = 0;
-            if (j != 0) left = ((testMatrix[i - 1][j - 1]) * 1.4);
+            if (j != 0) left = ((newMatrix[i - 1][j - 1].weightSum) * 1.4);
 
             if(j == 0)
             {
-                if(up < right)
+                if(up <= right)
                 {
                     newMatrix[i][j].weightSum += up;
                     newMatrix[i][j].dirPath = 0;
@@ -77,7 +86,7 @@ int main() {
             //If in right-most column (only compare left and up)
             else if(j == col)
             {
-                if(up < left)
+                if(up <= left)
                 {
                     newMatrix[i][j].weightSum += up;
                     newMatrix[i][j].dirPath = 0;
@@ -91,7 +100,7 @@ int main() {
             //Any columns inbeetween
             else
             {
-                if(up < right)
+                if(up <= right)
                 {
                     if(left < up)
                     {
@@ -100,16 +109,16 @@ int main() {
                     }
                     else
                     {
-                        newMatrix[i][j].weightSum = up;
+                        newMatrix[i][j].weightSum += up;
                         newMatrix[i][j].dirPath = 0;
                     }
                 }
                 else
                     if( right < left)
-                {
+                    {
                     newMatrix[i][j].weightSum += right;
                     newMatrix[i][j].dirPath = 1;
-                }
+                    }
                     else
                     {
                         newMatrix[i][j].weightSum += left;
@@ -125,7 +134,7 @@ int main() {
     {
         for(j = 0; j < col; j++)
         {
-            cout << newMatrix[i][j].weightSum << "  ";
+            cout << setw(3) << newMatrix[i][j].weightSum << "  ";
         }
         cout << endl;
     }
@@ -134,7 +143,7 @@ int main() {
     {
         for(j = 0; j < col; j++)
         {
-            cout << newMatrix[i][j].dirPath << "  ";
+            cout << setw(3) << newMatrix[i][j].dirPath << "  ";
         }
         cout << endl;
     }
